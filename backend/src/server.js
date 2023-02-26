45db27d2770a04b0d2863913536774c78c0a24da
Matt <lumpychum@gmail.com>
2023-02-25 18:47:47 -0600

    deployment stuff

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/v1/restaurants', restaurants)
+app.use('/api/v1', (req,res) => res.status(200).json({status: 'success'}))
 app.use('*', (req, res) => res.status(404).json({error: 'not found'}))
 
 export default app
