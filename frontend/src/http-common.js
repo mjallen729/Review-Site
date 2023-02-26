af7ddabb074793c686e686e75b05643232ac3473
Matt <lumpychum@gmail.com>
2023-02-25 19:02:10 -0600

    connected frontend to api server

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: 'http://localhost:5100/api/v1/restaurants',
+	baseURL: 'https://review-site-bn8x.onrender.com/api/v1/restaurants',
 	headers: {
 		'Content-type': 'application/json'
 	}
